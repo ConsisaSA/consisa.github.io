fc3ecb9e9249168e43aea4465501a51d4cd8b743
JoseReyes1504 <88816110+JoseReyes1504@users.noreply.github.com>
2023-10-12 15:52:23 -0600

    Diseño de paginas LS, BC, Dynamics, Fabrica

---
+++ CONSISA/src/js/Enlaces.js
@@ -0,0 +1,17 @@
+var LS = document.getElementById("LS");
+var BC = document.getElementById("BC");
+var Dynamics = document.getElementById("Dynamics");
+var Fabrica = document.getElementById("Fabrica");
+var Inicio = document.getElementById("index");
+
+function URLS(Boton, URL){
+    Boton.addEventListener("click", () =>{
+        location.href = URL;
+    });    
+}
+
+URLS(LS, "./CONSISA/src/html/lsRetail.html");
+URLS(BC, "./CONSISA/src/html/dynamics-FO.html");
+URLS(Dynamics, "./CONSISA/src/html/dynamics365.html");
+URLS(Fabrica, "./CONSISA/src/html/fabrica.html");
+URLS(Inicio, "./index.html");
