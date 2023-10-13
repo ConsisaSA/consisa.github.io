07119f0e3cb2b0cd1c56b1d49d40c19384addbe7
JoseReyes1504 <88816110+JoseReyes1504@users.noreply.github.com>
2023-10-13 10:00:12 -0600

    Enlaces

--- CONSISA/src/js/Enlaces.js
+++ CONSISA/src/js/Enlaces.js
@@ -2,7 +2,6 @@ var LS = document.getElementById("LS");
 var BC = document.getElementById("BC");
 var Dynamics = document.getElementById("Dynamics");
 var Fabrica = document.getElementById("Fabrica");
-var Inicio = document.getElementById("index");
 
 function URLS(Boton, URL){
     Boton.addEventListener("click", () =>{
@@ -14,4 +13,3 @@ URLS(LS, "./CONSISA/src/html/lsRetail.html");
 URLS(BC, "./CONSISA/src/html/dynamics-FO.html");
 URLS(Dynamics, "./CONSISA/src/html/dynamics365.html");
 URLS(Fabrica, "./CONSISA/src/html/fabrica.html");
-URLS(Inicio, "./index.html");
--- CONSISA/src/js/Header.js
+++ CONSISA/src/js/Header.js
@@ -1,13 +1,13 @@
 let animado = document.querySelectorAll('.Animado');
 const style = document.documentElement.style;
 var Toques = 0;
+var Inicio = document.getElementById("index");
 
-
-function mostrarScroll(){
+function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
-    for(var i = 0; i<animado.length; i++){
+    for (var i = 0; i < animado.length; i++) {
         let AlturaAnimado = animado[i].offsetTop;
-        if(AlturaAnimado -500 < scrollTop){
+        if (AlturaAnimado - 500 < scrollTop) {
             animado[i].style.opacity = 1;
             animado[i].classList.add("mostrarArriba");
         }
@@ -17,6 +17,9 @@ function mostrarScroll(){
 window.addEventListener('scroll', mostrarScroll);
 
 
+Inicio.addEventListener("click", () => {
+    window.location.href = "../../../index.html";
+});
 
 
 
