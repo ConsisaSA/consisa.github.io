0bd73fdb5fd722fff879bc216caac5df0fc659ae
JoseReyes1504 <88816110+JoseReyes1504@users.noreply.github.com>
2023-10-19 22:27:24 -0600

    Enlaces

--- CONSISA/src/js/Enlaces.js
+++ CONSISA/src/js/Enlaces.js
@@ -2,6 +2,12 @@ var LS = document.getElementById("LS");
 var BC = document.getElementById("BC");
 var Dynamics = document.getElementById("Dynamics");
 var Fabrica = document.getElementById("Fabrica");
+var Arquitectura = document.getElementById("Arquitectura");
+var Data = document.getElementById("Data");
+var Seguridad = document.getElementById("Seguridad");
+var Outsourcing = document.getElementById("Outsourcing");
+var Idera = document.getElementById("Idera");
+var Aranda = document.getElementById("Aranda");
 
 function URLS(Boton, URL){
     Boton.addEventListener("click", () =>{
@@ -12,4 +18,10 @@ function URLS(Boton, URL){
 URLS(LS, "./CONSISA/src/html/lsRetail.html");
 URLS(BC, "./CONSISA/src/html/dynamics-FO.html");
 URLS(Dynamics, "./CONSISA/src/html/dynamics365.html");
+URLS(Arquitectura, "./CONSISA/src/html/arquitectura.html");
 URLS(Fabrica, "./CONSISA/src/html/fabrica.html");
+URLS(Data, "./CONSISA/src/html/datamanagement.html");
+URLS(Seguridad, "./CONSISA/src/html/seguridad.html");
+// URLS(Outsourcing, "./CONSISA/src/html/fabrica.html");
+URLS(Idera, "./CONSISA/src/html/idera.html");
+URLS(Aranda, "./CONSISA/src/html/aranda.html");
